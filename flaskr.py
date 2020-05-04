53fb3cfbf22353461866c7eac9fc6b5986038963
Erik Ferreira <ekdespe@gmail.com>
2020-05-04 20:09:11 +0100

    sanitize

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from pymongo import MongoClient, DESCENDING, ASCENDING
 from bson.objectid import ObjectId
+from bson import json_util
 import settings as st
 from flask_cors import CORS
 import json
@@ -21,9 +22,7 @@ tb_agenda = conn.agenda
 ############################
 
 
-
-
-#cadastra novo cliente
+#_cadastra novo cliente
 @app.route('/cliente/create', methods = ['GET','POST'])
 def new_cliente():
     response = request.args
@@ -44,7 +43,7 @@ def new_cliente():
         return jsonify(result="error")
 
 
-#seta perfil cliente
+#_seta perfil cliente
 @app.route('/cliente/set/perfil', methods = ['POST'])
 def cliente_set_perfil():
     response = request.args
@@ -63,7 +62,7 @@ def cliente_set_perfil():
         return jsonify(result="error")
 
 
-#seta mentor cliente
+#_seta mentor cliente
 @app.route('/cliente/mentores/<id>', methods = ['POST'])
 def get_mentores(id):
 
@@ -104,7 +103,7 @@ def get_mentores(id):
         return jsonify(result="error")
 
 
-#desvincula profissional de cliente
+#_desvincula profissional de cliente
 @app.route('/cliente/remover/profissional/<id>', methods = ['GET','POST'])
 def cliente_remove_profissional(id):
     bd_response = collection_cliente.update_one({
@@ -119,7 +118,7 @@ def cliente_remove_profissional(id):
     else:
         return jsonify(result="error")
 
-#vincula profissional ao cliente
+#_vincula profissional ao cliente
 @app.route('/cliente/<id_cliente>/adicionar/profissional/<id_profissional>', methods = ['GET','POST'])
 def cliente_adicionar_profissional(id_cliente,id_profissional):
     bd_response = collection_cliente.update_one({
@@ -134,15 +133,11 @@ def cliente_adicionar_profissional(id_cliente,id_profissional):
     else:
         return jsonify(result="error")
 
-#Register new professional
-@app.route('/profissionais/new', methods = ['GET', 'POST'])
-def new():
-     return render_template('/main/profissional/new.html')
 
-#Create new profissional
+#_Create new profissional
 @app.route('/profissionais/create',  methods=['GET', 'POST'])
 def create():
-    collection.insert_one({
+    bd_response = collection.insert_one({
         'nome': request.form['nome'],
         'idade': request.form['idade'],
         'sexo': request.form['sexo'],
@@ -151,44 +146,84 @@ def create():
         'email': request.form['email'],
         'especialidade': request.form['especialidade'],
     })
+    
+    if bd_response:
+       
+        return jsonify(result="sucess"),200
+    else:
+        return jsonify(result="error")
 
-    return redirect(url_for('index'))
 
-#List professionals
+
+
+#_List professionals
 @app.route('/profissionais', methods=['GET', 'POST'])
 def findAll():
-    list_collection  = collection.find()
-    return render_template('/main/profissional/list.html', profissionais = list_collection )
+    bd_response  = collection.find()
+    if bd_response:
+        result   = []
+        for i in bd_response:
+            result.append(i)
 
-#Searching for a professional by ID
+        result = json.loads(json_util.dumps(result))
+        return jsonify(result=result),200
+    else:
+        return jsonify(result="error")
+
+#_Searching for a professional by ID
 @app.route('/profissionais/<id>', methods=['GET', 'POST'])
 def findOne(id):
     data = collection.find_one({
         "_id": ObjectId(id)
     })
-    return render_template('/main/profissional/viewProfisisonal.html', profissional = data)
+    if bd_response:
+        page_sanitized = json.loads(json_util.dumps(bd_response))
+        return jsonify(result=page_sanitized),200
+    else:
+        return jsonify(result="error")
 
-#Exclude one professional By ID
+#_Searching client by email
+@app.route('/cliente/<email>', methods=['GET', 'POST'])
+def findOne_by_email(email):
+    bd_response = collection_cliente.find_one({
+        "email": email
+    })
+    
+    if bd_response:
+        page_sanitized = json.loads(json_util.dumps(bd_response))
+        return jsonify(result=page_sanitized),200
+    else:
+        return jsonify(result="error")
+
+#_Exclude one professional By ID
 @app.route('/profissionais/delete/<id>', methods=['GET', 'POST', 'DELETE'])
 def delete(id):
-    data = collection.delete_many({
+    bd_response = collection.delete_many({
         "_id": ObjectId(id)
     })
+    if bd_response:
+        
+        return jsonify(result="sucess"),200
+    else:
+        return jsonify(result="error")
 
-    return redirect(url_for('findAll'))
 
-#Show a profissional to edition
+#_Show a profissional to edition
 @app.route('/profissionais/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
     data = collection.find_one({
         "_id": ObjectId(id)
     })
-    return render_template('/main/profissional/edit.html', profissional = data)
+    if bd_response:
+        page_sanitized = json.loads(json_util.dumps(bd_response))
+        return jsonify(result=page_sanitized),200
+    else:
+        return jsonify(result="error")
 
-#Edit a profissional By ID
+#_Edit a profissional By ID
 @app.route('/profissionais/update/<id>', methods=['GET', 'POST', 'PUT'])
 def update(id):
-    collection.update_one({
+    bd_response = collection.update_one({
             "_id": ObjectId(id)
         },
         {
@@ -203,58 +238,53 @@ def update(id):
             }
         }
     )
+    if bd_response:
+        return jsonify(result="sucess"),200
+    else:
+        return jsonify(result="error")
 
-    return redirect(url_for('findAll'))
 ###########
-###########
-###########
-#Create professional assessment
+#_Create professional assessment
 @app.route('/profissinais/newRating',  methods=['GET', 'POST'])
 def createCliente():
-    tb_avaliacao.insert_one({
+   bd_response = tb_avaliacao.insert_one({
         'idPorfissional': 3,
         'idCliente': 3,
         'rating': 7
     })
+   if bd_response:
+       return jsonify(result="sucess"),200
+   else:
+       return jsonify(result="error")
 
-    return redirect(url_for('index'))
 ###########
 
-#Redirect to Clinet's register
-@app.route('/cliente/new', methods = ['GET', 'POST'])
-def newCliente():
-     return render_template('/main/cliente/new.html')
 
 ############################
 #Others route
 
-############################
-#Route to create Login's screen
-@app.route('/', methods = ['GET'])
-def index():
-    return render_template('/main/login.html')
 
-#HOME's route
+#_HOME's route
 @app.route('/profissionais/home', methods = ['GET'])
 def home():
     #Consulta par exibir todas os mais bem avaliados
     profissionais = tb_avaliacao.find().sort("rating", DESCENDING).limit(4)
-    return render_template('/main/profissional/home.html', profissionais_by_rating = profissionais)
+    if bd_response:
+        result   = []
+        for i in bd_response:
+            result.append(i)
 
-#Schedule's route
-@app.route('/profissionais/agenda', methods = ['GET'])
-def agenda():
-    return render_template('/main/profissional/agenda.html')
+        result = json.loads(json_util.dumps(result))
+        return jsonify(result=result),200
+    else:
+        return jsonify(result="error")
 
-#Route to create schedule
-@app.route('/profissionais/agenda/new', methods = ['GET'])
-def newAgenda():
-    return render_template('/main/profissional/newAgenda.html')
 
-#Route to save schedule
+
+#_Route to save schedule
 @app.route('/profissionais/agenda/save', methods = ['GET', 'POST'])
 def saveAgenda():
-    tb_agenda.insert_one({
+    bd_response= tb_agenda.insert_one({
         'idPorfissional': 1,
         'clientesAgendados': {
             'idCliente': 1,
@@ -262,21 +292,30 @@ def saveAgenda():
             'horario_fim': request.form['horario_fim'],
         }
     })
-    return redirect(url_for('home'))
+    if bd_response:
+        return jsonify(result="sucess"),200
+    else:
+        return jsonify(result="error")
+    
 
 
-#Route to edit schedule
+#_Route to edit schedule
 @app.route('/profissionais/agenda/edit/<id>', methods=['GET', 'POST'])
 def editAgenda(id):
-    data = collection.find_one({
+    bd_response = collection.find_one({
         "_id": ObjectId(id)
     })
     return render_template('/main/profissional/editAgenda.html', profissional = data)
+    if bd_response:
+        page_sanitized = json.loads(json_util.dumps(bd_response))
+        return jsonify(result=page_sanitized),200
+    else:
+        return jsonify(result="error")
 
-#Route to update schedule
+#_Route to update schedule
 @app.route('/profissionais/agenda/update', methods = ['GET', 'POST', 'PUT'])
 def updateAgenda(id):
-    tb_agenda.update_one(
+    bd_response = tb_agenda.update_one(
         {'_id': ObjectId(id)},
         {
             '$set':{
@@ -287,16 +326,25 @@ def updateAgenda(id):
             }
         }
     )
-    return redirect(url_for('home'))
+    if bd_response:
+        
+        return jsonify(result="sucess"),200
+    else:
+        return jsonify(result="error")
 
-#Exclude a professional's schedule By ID
+#_Exclude a professional's schedule By ID
 @app.route('/profissionais/agenda/delete/<id>', methods=['GET', 'POST', 'DELETE'])
 def deleteAgenda(id):
-    data = tb_agenda.delete_many({
+    bd_response = tb_agenda.delete_many({
         "_id": ObjectId(id)
     })
 
-    return redirect(url_for('findAll'))
+    if bd_response:
+        page_sanitized = json.loads(json_util.dumps(bd_response))
+        return jsonify(result=page_sanitized),200
+    else:
+        return jsonify(result="error")
+    
 
 #endrotas
 
